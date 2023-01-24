3c815b87f7ed6a044c8042cc03aeaf23b1e7715e
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-01-24 09:28:28 +0300

    form data to server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const url = "http://localhost:3000/users";
+
+const form = document.querySelector(".info");
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const dataForm = new FormData(form);
+    const data = Object.fromEntries(dataForm);
+
+    fetch(`${url}`, {
+        method: "POST",
+        headers: {
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify(data)
+
+    });
+})
+
 
